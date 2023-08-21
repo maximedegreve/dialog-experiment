7d2dd2bbf1286a8cbb2cda582337f446b4bad990
Maxime De Greve <hello@maximedegreve.com>
2023-08-21 17:21:46 +0000

    Upgrade to latest react

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import App from './App.js'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-const container = document.getElementById('root')
-const root = createRoot(container)
+import App from './App'
+import './reset.css'
+
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
 
 root.render(
-    <React.StrictMode>
+    <StrictMode>
         <App />
-    </React.StrictMode>
+    </StrictMode>
 )
-root.unmount()
